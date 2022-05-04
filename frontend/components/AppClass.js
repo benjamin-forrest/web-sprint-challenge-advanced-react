767926182e3ac01a7318a16a118548c5cd592ffc
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-05-04 10:54:02 -0700

    axios

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Axios from 'axios'
 
 export default class AppClass extends React.Component {
   constructor(){
@@ -12,6 +13,48 @@ export default class AppClass extends React.Component {
       email: "",
     };
   }
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (!this.state.email.length) {
+      return this.setState({ message: "Ouch: email is required" });
+    } else if (this.state.email.split('.').length !== 2) {
+      return this.setState({ message: "Ouch: email must be a valid email" });
+    } else if (this.state.email === "foo@bar.baz") {
+      return this.setState({
+        message: "foo@bar.baz failure #71",
+        grid: [0, 0, 0, 0, "B", 0, 0, 0, 0],
+        steps: 0,
+        x: 2,
+        y: 2,
+        email: ""
+      });
+  } else {
+    const url = "http://localhost:9000/api/result";
+    Axios.post(url, {
+      x: this.state.x,
+      y: this.state.y,
+      steps: this.state.steps,
+      email: this.state.email,
+    })
+      .then((res) => {
+        this.setState({
+          message: res.data.message,
+        });
+        document.getElementById("email").value = "";
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          message: err.message,
+          grid: [0, 0, 0, 0, "B", 0, 0, 0, 0],
+          steps: 0,
+          x: 2,
+          y: 2,
+          email: ""
+        });
+      });
+  }
+};
   render() {
     const { className } = this.props
     return (
