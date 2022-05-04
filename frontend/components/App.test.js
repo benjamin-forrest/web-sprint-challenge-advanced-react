2ad854487080398d609c66aa8f8e7b2f455e8258
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-05-04 13:19:23 -0700

    tests for page

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,31 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react'
+import {fireEvent, render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import App from './AppClass'
+
+const leftButton = () => screen.getByText('LEFT')
+const upButton = () => screen.getByText('UP')
+const rightButton = () => screen.getByText('RIGHT')
+const downButton = () => screen.getByText('DOWN')
+const resetButton = () => screen.getByText('reset')
+const emailField = () => screen.getByText('type email')
+beforeEach(() =>{
+  render(<App/>)
+})
+describe('student tests', () => {
+  test('rendering', ()=>{
+    screen.debug()
+  })
+  test('can see it all', () => {
+    expect(leftButton()).toBeInTheDocument()
+    expect(upButton()).toBeInTheDocument()
+    expect(rightButton()).toBeInTheDocument()
+    expect(downButton()).toBeInTheDocument()
+    expect(resetButton()).toBeInTheDocument()
+    expect(emailField()).toBeInTheDocument()
+  })
+  test('inputting an email and double checking value passes through', () =>{
+    fireEvent.change(emailField(), {target: {value: 'lady@gaga.com'}});
+    expect(emailField()).toHaveValue('lady@gaga.com')
+  })
 })
