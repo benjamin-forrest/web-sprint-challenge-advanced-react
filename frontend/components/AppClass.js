bcfacf74a8f8cda58dd835acaa116539c6681d01
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-05-04 11:20:43 -0700

    fixed spacing for tests

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -88,7 +88,7 @@ getPosition = () => {
             Coordinates ({this.state.x},{this.state.y})</h3>
           <h3 id="steps">
             You moved {this.state.steps} 
-            time{`${this.state.steps === 1 ? "" : "s"}`} </h3>
+             time{`${this.state.steps === 1 ? "" : "s"}`} </h3>
         </div>
         <div id="grid">
         {this.state.grid.map((square, i) => (
